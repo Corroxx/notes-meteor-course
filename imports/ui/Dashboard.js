22999779136715dfa6ee3eb2e9694eb286a994e9
Andreas Bühler <andreasbuehler@me.com>
2017-04-21 14:13:30 +0200

    minor startup bugfix

--- imports/ui/Dashboard.js
+++ imports/ui/Dashboard.js
@@ -1,10 +1,8 @@
 import React from 'react';
 
 
-import LinksList from './LinksList';
 import PrivateHeader from './PrivateHeader';
 import {AddLink} from './AddLink';
-import LinksListFilters from './LinksListFilters';
 
 export default () => {
     return (

1bfc342df3c643b6ec4b1fc3ebb912eef4778c79
Andreas Bühler <andreasbuehler@me.com>
2017-08-30 23:58:44 +0200

    Integrated selectedNoteId into list items

--- client/main.js
+++ client/main.js
@@ -1,6 +1,7 @@
 import {Meteor} from 'meteor/meteor';
 import ReactDOM from 'react-dom';
 import { Tracker } from 'meteor/tracker';
+import { browserHistory } from 'react-router';
 
 
 import {onAuthChange, routes} from '../imports/routes/routes';
@@ -12,7 +13,15 @@ Tracker.autorun(() => {
   onAuthChange(isAuthenticated);
 });
 
+Tracker.autorun(() => {
+ const selectedNoteId = Session.get('selectedNoteId');
+
+ if(selectedNoteId) {
+   browserHistory.replace(`/dashboard/${selectedNoteId}`);
+ }
+});
 
 Meteor.startup(() => {
+  Session.set('selectedNoteId', undefined);
   ReactDOM.render(routes, document.getElementById('app'));
 })
---
+++ imports/fixtures/fixtures.js
@@ -0,0 +1,16 @@
+export const notes = [
+    {
+        _id: 'noteId1',
+        title: 'Test Title',
+        body: 'something',
+        updatedAt: 1503524447788,
+        userId: 'userId1'
+    },
+    {
+        _id: 'noteId2',
+        title: '',
+        body: 'something is here',
+        updatedAt: 1503524447788,
+        userId: 'userId2'
+    }
+];

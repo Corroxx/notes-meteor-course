fe5735e4deb9a925fc3776363da2d9c90a37084d
Andreas Bühler <andreasbuehler@me.com>
2017-08-31 21:32:22 +0200

    Mark selected note in list

--- imports/routes/routes.js
+++ imports/routes/routes.js
@@ -20,6 +20,13 @@ const onEnterPrivatePage= () => {
     browserHistory.replace("/");
   }
 }
+const onEnterNotePage= (nextState) => {
+  if(!Meteor.userId()) {
+    browserHistory.replace("/");
+  } else {
+   Session.set('selectedNoteId', nextState.params.id);
+  }
+}
 
 export const onAuthChange = (isAuthenticated) => {
   const pathname = browserHistory.getCurrentLocation().pathname;
@@ -38,7 +45,7 @@ export const routes = (
     <Route path="/" component={Login} onEnter={onEnterPublicPage}/>
     <Route path="/signup" component={Signup} onEnter={onEnterPublicPage}/>
     <Route path="/dashboard" component={Dashboard} onEnter={onEnterPrivatePage}/>
-    <Route path="/dashboard/:id" component={Dashboard} onEnter={onEnterPrivatePage}/>
+    <Route path="/dashboard/:id" component={Dashboard} onEnter={onEnterNotePage}/>
     <Route path="*" component={NotFound}/>
   </Router>
 );
